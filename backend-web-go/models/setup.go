5e027c0b5c586d328f4165461478cde6d5c1a3c2
kshitiz-shresth <er.kshitizshrestha@gmail.com>
2022-07-05 13:11:09 +0545

    [feat] use of .env to connect database credentials

--- backend-web-go/models/setup.go
+++ backend-web-go/models/setup.go
@@ -2,19 +2,26 @@ package models
 
 import (
 	"fmt"
+	"log"
+	"os"
 
 	// go-sql-driver/mysql
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/jinzhu/gorm"
+	"github.com/joho/godotenv"
 )
 
 // SetupDB : initializing mysql database
 func SetupDB() *gorm.DB {
-	USER := "root"
-	PASS := ""
+	envErr := godotenv.Load()
+	if envErr != nil {
+		log.Fatal("Error loading .env file")
+	}
+	USER := os.Getenv("DB_USERNAME")
+	PASS := os.Getenv("DB_PASSWORD")
 	HOST := "localhost"
 	PORT := "3306"
-	DBNAME := "rtw_rnd_golang_crud"
+	DBNAME := os.Getenv("DB_NAME")
 	URL := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local", USER, PASS, HOST, PORT, DBNAME)
 	db, err := gorm.Open("mysql", URL)
 	if err != nil {
