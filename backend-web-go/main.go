eaef6a23473337eebcd8888810bab1b5d9a0e2cf
kshitiz-shresth <er.kshitizshrestha@gmail.com>
2022-07-05 12:33:38 +0545

    [feat] backend web golang with basic task crud

---
+++ backend-web-go/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"github.com/kshitiz-shresth/go-react-cms/backend-web-go/models"
+	"github.com/kshitiz-shresth/go-react-cms/backend-web-go/routes"
+)
+
+func main() {
+
+	db := models.SetupDB()
+	db.AutoMigrate(&models.Task{})
+
+	r := routes.SetupRoutes(db)
+	r.Run()
+}
